b60d40eeddf68fe2cae3cf1e167fa3ff2601e05c
meta-bay <bayiemeseret@gmail.com>
2024-09-02 13:11:14 +0300

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,12 +1,10 @@
 import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = createClient();
     this.client.on('error', (err) => console.log(err));
-    this.client.connect().catch((err) => {
-      console.log(err);
-    });
   }
 
   isAlive() {
@@ -14,17 +12,18 @@ class RedisClient {
   }
 
   async get(key) {
-    return await this.client.get(key);
+    const getVal = promisify(this.client.get).bind(this.client);
+    return getVal(key);
   }
 
   async set(key, val, expTime) {
-    await this.client.set(key, val, {
-      EX: expTime,
-    });
+    const setVal = promisify(this.client.set).bind(this.client);
+    setVal(key, val, 'EX', expTime);
   }
 
   async del(key) {
-    await this.client.del(key);
+    const delPair = promisify(this.client.del).bind(this.client);
+    delPair(key);
   }
 }
 
